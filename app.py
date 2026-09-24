2520fdbad67e98cd0ac8fa0337e905d9e0b1e3c4
lukasmartincornejo-creator <lukasmartincornejo@gmail.com>
2026-09-24 20:06:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -308,7 +308,7 @@ with col_h_title:
 
     st.title("Tablero Ejecutivo: Monitoreo Voz del Cliente (VdC)")
 
-    st.markdown("**Grido Argentina** | Análisis automatizado de experiencia de usuario y fricción - App Store")
+    st.markdown(| Análisis automatizado de experiencia de usuario y fricción - App Store")
 
 
 
@@ -738,7 +738,7 @@ with tab_categorias:
 
 
 
-# --- TAB 4: EXPLORADOR DE COMENTARIOS CON TABLA HTML BLANCA PURA ---
+# --- TAB 4: EXPLORADOR DE COMENTARIOS  ---
 
 with tab_explorador:
 
